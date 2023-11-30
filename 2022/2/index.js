0655cbd1a3d8b8c7121a88fbf878c01dc49aa7f5
Manish Shrestha <manishrestha529@gmail.com>
2023-11-30 23:22:41 +0000

    day 2 part 2

--- 2022/2/index.js
+++ 2022/2/index.js
@@ -1,23 +1,43 @@
 const {readFileSync} = require('fs')
 
-const solution = (input) => {
+const solution = (input, cheat=false) => {
 
     // rock = a, x
     // paper = b, y
     // scissors = c, z
-
     const scoreTable = {
         A: {X: 3, Y: 6, Z: 0},
         B: {X: 0, Y: 3, Z: 6},
         C: {X: 6, Y: 0, Z: 3}
     }
-
     const movePoints = {X:1, Y:2, Z:3}
 
+    // X: lose
+    // Y: draw
+    // Z: win
+    const scoreTableCheat = {
+        A: {X: 'Z', Y: 'X', Z: 'Y'},
+        B: {X: 'X', Y: 'Y', Z: 'Z'},
+        C: {X: 'Y', Y: 'Z', Z: 'X'}
+    };
+    const gameScoreCheat = {X:0, Y:3, Z:6};
+
     const getScore = (game) => {
-        const [opponent, me] = game.split(' '); 
-        return scoreTable[opponent][me]+movePoints[me];
+        const [opponent, me] = game.split(' ');
+        let gameScore=0;
+        let movePoint=0;
+
+        if(cheat){
+             gameScore = gameScoreCheat[me];
+             movePoint = movePoints[scoreTableCheat[opponent][me]];
+        } else {
+             gameScore = scoreTable[opponent][me];
+             movePoint = movePoints[me]
+        }
+
+        return gameScore+movePoint;
     }
+
     
     return input.split('\n').map(game => getScore(game)).reduce((a,b)=>a+b);
 }
@@ -26,3 +46,4 @@ module.exports = { solution }
 
 const input = readFileSync('input-real.txt').toString()
 console.log('answer 1: ', solution(input));
+console.log('answer 2: ', solution(input, true));
--- 2022/2/index.test.js
+++ 2022/2/index.test.js
@@ -5,6 +5,9 @@ const input = readFileSync('./input-test.txt').toString()
 
 
 test('part 1', ()=>{
-
     expect(solution(input)).toBe(15);
+})
+
+test('part 2', ()=>{
+    expect(solution(input, true)).toBe(12);
 })
